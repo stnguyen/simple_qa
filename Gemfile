282925dd702388effd4c8b21e6d8ab0d40bfdc7f
Stefan N <stefan@vinova.sg>
2011-02-23 16:18:50 +0700

    init

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+
+gem 'mongoid', '2.0.0.rc.6'
+gem 'bson_ext', '~> 1.2'
+
+gem 'nifty-generators'
+gem 'voteable_mongoid'
+gem 'devise'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleQa::Application.config.secret_token = '1a920d30a0529a74b9a2a5c93dee3243d0741790113b963ea836cd35b9a88cb22cfee82511a96b86bebfaa5a6db7c1dfa0b893e38a218ba10344d3159837c787'
