46a67eb2cb45834c8074579d7f8b31d400478797
Stefan N <stefan@vinova.sg>
2011-02-23 19:29:12 +0700

    question#index

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,24 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+end
