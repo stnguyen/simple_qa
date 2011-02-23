af8ab98e19e574bea29c3a383fcca0d5e8a9b216
Stefan N <stefan@vinova.sg>
2011-02-23 17:24:56 +0700

    make Question & Answer votable, set voting strategies

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,11 @@
 class Answer
   include Mongoid::Document
+  include Mongoid::Voteable
+  
   field :content, :type => String
   
   referenced_in :question
+  
+  vote_point self, :up => +1, :down => -2
+  vote_point Question, :up => +2, :down => -1
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,11 @@
 class Question
   include Mongoid::Document
+  include Mongoid::Voteable
+  
   field :title, :type => String
   field :body, :type => String
   
   references_many :answers
+  
+  vote_point self, :up => +1, :down => -1
 end
